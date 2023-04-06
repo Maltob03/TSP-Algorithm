d1f578a0082e5edb2f1628f2049eb004d6187207
Maltob03 <46380730+Maltob03@users.noreply.github.com>
2023-04-06 15:05:18 +0200

    added button to reset and generate Annotation

--- TSP Algortithm.swiftpm/ContentView.swift
+++ TSP Algortithm.swiftpm/ContentView.swift
@@ -3,31 +3,50 @@ import MapKit
 
 struct ContentView: View {
     @State private var annotations: [MKAnnotation] = []
+    @State var cities = [
+        ("New York", CLLocation(latitude: 40.7128, longitude: -74.0060)),
+        ("Houston", CLLocation(latitude: 29.7604, longitude: -95.3698)),
+        ("Phoenix", CLLocation(latitude: 33.4484, longitude: -112.0740)),
+        ("San Diego", CLLocation(latitude: 32.7157, longitude: -117.1611)),
+        ("Dallas", CLLocation(latitude: 32.7767, longitude: -96.7970)),
+        ("San Jose", CLLocation(latitude: 37.3382, longitude: -121.8863)),
+        ("Los Angeles", CLLocation(latitude: 34.0522, longitude: -118.2437)),
+        ("Chicago", CLLocation(latitude: 41.8781, longitude: -87.6298)),
+        ("Philadelphia", CLLocation(latitude: 39.9526, longitude: -75.1652)),
+    ]
     
     var body: some View {
-        MapView(annotations: $annotations)
-            .onAppear {
-                // Define the cities to visit
-                let cities = [
-                    ("New York", CLLocation(latitude: 40.7128, longitude: -74.0060)),
-                    ("Houston", CLLocation(latitude: 29.7604, longitude: -95.3698)),
-                    ("Phoenix", CLLocation(latitude: 33.4484, longitude: -112.0740)),
-                    ("San Diego", CLLocation(latitude: 32.7157, longitude: -117.1611)),
-                    ("Dallas", CLLocation(latitude: 32.7767, longitude: -96.7970)),
-                    ("San Jose", CLLocation(latitude: 37.3382, longitude: -121.8863))
-                ]
-                
-                // Find the shortest path and plot it on the map
-                if let shortest = shortestPath(cities: cities) {
-                    annotations = shortest.path.map { location -> MKAnnotation in
-                        let annotation = MKPointAnnotation()
-                        annotation.coordinate = location.coordinate
-                        return annotation
-                    }
-                }
+        VStack{
+            MapView(annotations: $annotations).ignoresSafeArea()
+            buttons
             }
     }
+    
+    var buttons: some View{
+        Group{
+            HStack{
+                Button("Generate") {
+                    // Find the shortest path and plot it on the map
+                    if let shortest = shortestPath(cities: cities) {
+                        annotations = shortest.path.map { location -> MKAnnotation in
+                            let annotation = MKPointAnnotation()
+                            annotation.coordinate = location.coordinate
+                            return annotation
+                        }
+                    }
+                }.buttonStyle(BorderedProminentButtonStyle())
+                    .padding(24)
+                Spacer()
+                Button("Reset") {
+                    annotations = []
+                }.buttonStyle(BorderedProminentButtonStyle())
+                .padding(24)
+            }
+        }
+    }
+    
 }
+
 struct MapView: UIViewRepresentable {
     @Binding var annotations: [MKAnnotation]
     
@@ -41,6 +60,8 @@ struct MapView: UIViewRepresentable {
         mapView.removeAnnotations(mapView.annotations)
         mapView.addAnnotations(annotations)
         
+        // Remove any overlays (i.e., lines connecting cities)
+        mapView.removeOverlays(mapView.overlays)
         
         if annotations.count > 1 {
             let coordinates = annotations.map { $0.coordinate }
@@ -91,4 +112,5 @@ struct MapView: UIViewRepresentable {
         }
     }
 }
+
 
